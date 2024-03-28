02a46416cdcf0ab6aa7770ba5d8a3e0209004ccc
Jino5 <japino83@gmail.com>
2024-03-28 17:13:35 +0900

    Props type BookmarkResponse change

--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -1,22 +1,23 @@
 
 import { getAllBookmarks } from "@/lib/bookmarkApi";
-import { BookmarkType, BookmarksResponse } from "@/types";
+import { BookmarksResponse } from "@/types";
+import Bookmarks from '../components/Bookmarks';
 
-export default async function BookmarkPage()  {
-  const bookmarksData: Promise<BookmarksResponse> = getAllBookmarks()
-  const { data } = await bookmarksData
-  const bookmarks: BookmarkType[] = data
+type Props = {
+  searchParams: { page: string }
+}
+
+export default async function BookmarkPage(props: Props)  {
+  const { page = 1 } = props.searchParams;
+  const bookmarksData: Promise<BookmarksResponse> = getAllBookmarks(parseInt(String(page)))
+  // const { data } = await bookmarksData
+  // const bookmarks: BookmarkType[] = data
+
+  const bookmarkRes = await bookmarksData
 
   return (
     <div>
-        <h1>Welcome to Bookmarker</h1>
-        {
-          bookmarks.map(bookmark => {
-            return (
-                <h2 key={bookmark.id}>{bookmark.id}{bookmark.title}</h2>
-            )
-          })
-        }
+        <Bookmarks bookmarks={bookmarkRes} />
     </div>
   );
 }
