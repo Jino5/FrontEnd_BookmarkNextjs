7f438da8523e5225d6e8c52a9abbc81d45ece191
Jino5 <japino83@gmail.com>
2024-03-29 09:39:16 +0900

    add bookmarkAPI evironment

--- src/lib/bookmarkApi.tsx
+++ src/lib/bookmarkApi.tsx
@@ -1,10 +1,17 @@
 import { BookmarksResponse } from "@/types"
 import axios from "axios"
 
-const API_BASE_URL = 'http://localhost:8080'
+// const API_BASE_URL = 'http://localhost:8080'
+import getConfig from 'next/config'
+const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
+
+const getApiUrl = () => {
+    return serverRuntimeConfig.API_BASE_URL || publicRuntimeConfig.API_BASE_URL;
+}
 
 export async function getAllBookmarks(page: number, query: string): Promise<BookmarksResponse> {
-    let API_URL = `${API_BASE_URL}/api/bookmarks?page=${page}`;
+    // let API_URL = `${API_BASE_URL}/api/bookmarks?page=${page}`;
+    let API_URL = `${getApiUrl()}/api/bookmarks?page=${page}`;
     if(query) {
         API_URL += `&query=${query}`
     }
